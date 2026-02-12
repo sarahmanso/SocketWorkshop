97834991b1b2bb34f369b50accd79873fe0871f7
sarahmanso <sarahmansor2580@gmail.com>
2026-02-12 10:48:18 +0200

    first models and schemas

---
+++ Backend/src/schemas/user_schema.py
@@ -0,0 +1,32 @@
+from pydantic import BaseModel, Field, ConfigDict
+from typing import Optional, List
+from enum import Enum
+
+from Backend.src.schemas.order_schema import OrderResponse
+
+class UserRole(str, Enum):
+    ADMIN = "admin"
+    USER = "user"
+
+
+class UserBase(BaseModel):
+    username: str = Field(..., min_length=1, max_length=100)
+    role: UserRole
+
+class UserCreate(UserBase):
+    password: str = Field(..., min_length=6, max_length=255)
+
+class UserUpdate(BaseModel):
+    username: Optional[str] = Field(None, min_length=1, max_length=100)
+    password: Optional[str] = Field(None, min_length=6, max_length=255)
+    role: Optional[UserRole] = None
+
+class UserInDB(UserBase):
+    id: int
+    model_config = ConfigDict(from_attributes=True)
+
+class UserResponse(UserInDB):
+    pass
+
+class UserWithOrders(UserResponse):
+    orders: List['OrderResponse'] = []

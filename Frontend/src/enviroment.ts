527b77b531b653a4c8c5bc8fdc95a96d442b2063
sarahmanso <sarahmansor2580@gmail.com>
2026-02-12 14:30:38 +0200

    components NAHHHHHHHHHHHHJAKQXD;OJ

---
+++ Frontend/src/enviroment.ts
@@ -0,0 +1,9 @@
+export const environment = {
+  production: false,
+  apiUrl: 'http://localhost:8000',
+  apiTimeout: 30000, 
+  tokenKey: 'auth_token',
+  userKey: 'user_data',
+};
+
+export default environment;
